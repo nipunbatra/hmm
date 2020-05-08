576047ed761407f2a61ffc48354850fc3b69a6be
unknown <rithwikkukunuri30@gmail.com>
2020-05-08 20:58:36 +0530

    Added the  autoplay

---
+++ em_calculator.py
@@ -0,0 +1,30 @@
+import numpy as numpy
+
+
+def compute_likelihood(sequence):
+
+
+def forward_algorithm()
+
+
+
+
+
+sequences = [['H','H','H'],
+			 ['H','T','H'],
+			 ['T','T','H'],
+			 ['H','H','T']]
+
+
+k = 2
+
+pi = np.random((k,1))
+
+A  = np.random((k,k))
+A  = A/np.sum(A,axis=1).reshape((-1,1))
+
+B = np.random((k,k))
+B = B/np.sum(B,axis=1).reshape((-1,1))
+
+
+
