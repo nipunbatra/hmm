f663f5f2cb72c1391e933a5660eb93d9b6eac225
unknown <rithwikkukunuri30@gmail.com>
2020-07-12 21:53:25 +0530

    Made changes for distill

---
+++ static/sigma.js/src/renderers/canvas/sigma.canvas.labels.def.js
@@ -0,0 +1,46 @@
+;(function(undefined) {
+  'use strict';
+
+  if (typeof sigma === 'undefined')
+    throw 'sigma is not declared';
+
+  // Initialize packages:
+  sigma.utils.pkg('sigma.canvas.labels');
+
+  /**
+   * This label renderer will just display the label on the right of the node.
+   *
+   * @param  {object}                   node     The node object.
+   * @param  {CanvasRenderingContext2D} context  The canvas context.
+   * @param  {configurable}             settings The settings function.
+   */
+  sigma.canvas.labels.def = function(node, context, settings) {
+    var fontSize,
+        prefix = settings('prefix') || '',
+        size = node[prefix + 'size'];
+
+    if (size < settings('labelThreshold'))
+      return;
+
+    if (typeof node.label !== 'string')
+      return;
+
+    fontSize = (settings('labelSize') === 'fixed') ?
+      settings('defaultLabelSize') :
+      settings('labelSizeRatio') * size;
+
+    context.font = (settings('fontStyle') ? settings('fontStyle') + ' ' : '') +
+        fontSize + 'px ' + settings('font');
+    context.fillStyle = (settings('labelColor') === 'node') ?
+      (node.color || settings('defaultNodeColor')) :
+      settings('defaultLabelColor');
+    var a = (node.angle || settings('angle') || 0) / 180 * Math.PI,
+      x = Math.round(node[prefix + 'x'] + (size + 3) * Math.cos(a)),
+      y = Math.round(node[prefix + 'y'] + (size + 3) * Math.sin(a));
+    context.save();
+    context.translate(x,y);
+    context.rotate(a);
+    context.fillText(node.label,0,0);
+    context.restore();
+  };
+}).call(this);
